a113e8b02e994b45a6847dc3823a0852a15eb630
Filipe Correia <filipe.lp.correia@gmail.com>
2021-03-24 08:41:09 +0000

    Adds watch something

--- Plugins/WatchSomething.js
+++ Plugins/WatchSomething.js
@@ -9,15 +9,42 @@
     "mediumLabel": "WatchSomething",
     "paletteLabel": "WatchSomething",
 }*/
-(() => {
-var action = new PlugIn.Action(/**
-     * @param {Selection} selection
-     */
-function(selection) {
-    let task = tagNamed("Watch").remainingTasks[0]
-    URL.fromString(task.note.trim()).open();
-    document.windows[0].selectObjects([task]);
-  });
+;(() => {
+  var action = new PlugIn.Action
+  /**
+   * @param {Selection} selection
+   */(function (selection) {
+    const form = new Form()
+    const watchTag = tagNamed('Watch')
+    const tasks = watchTag.remainingTasks
+    const tags = [
+      ...tasks
+        .reduce((acc, e) => {
+          e.tags.forEach((e) => acc.add(e))
+          return acc
+        }, new Set())
+        .values()
+    ]
 
-  return action;
-})();
+    const optionsField = new Form.Field.Option(
+      'tag',
+      'Which Tag you want to watch?',
+      tags,
+      tags.map((e) => e.name),
+      tags[0]
+    )
+
+    form.addField(optionsField)
+    form
+      .show("Here's what's available", 'Confirm')
+      .then(({ values: { tag } }) => {
+        const task = tag.remainingTasks.filter((e) =>
+          e.tags.some((e) => e == watchTag)
+        )[0]
+        URL.fromString(task.note.trim()).open()
+        document.windows[0].selectObjects([task])
+      })
+  })
+
+  return action
+})()
